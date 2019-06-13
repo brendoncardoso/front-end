6af0f24bbcd44b03e2b77675865c9b395bb948e5
Brendon Cardoso <brendoncarvalho22@gmail.com>
2019-06-13 17:46:10 -0300

    Aula de Banco de Dados

--- Aulas/b7web/b7web - Banco de Dados/b7web.sql
+++ Aulas/b7web/b7web - Banco de Dados/b7web.sql
@@ -16,6 +16,21 @@
 CREATE DATABASE IF NOT EXISTS `b7web` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `b7web`;
 
+-- Copiando estrutura para tabela b7web.faixas
+CREATE TABLE IF NOT EXISTS `faixas` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela b7web.faixas: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `faixas` DISABLE KEYS */;
+INSERT INTO `faixas` (`id`, `titulo`) VALUES
+	(1, '0 - 1000'),
+	(2, '1001 a 5000'),
+	(3, '5001 - 30000');
+/*!40000 ALTER TABLE `faixas` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela b7web.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -23,14 +38,19 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `email` varchar(255) NOT NULL,
   `senha` varchar(15) NOT NULL,
   `data_nascimento` date NOT NULL,
+  `contagem` int(11) NOT NULL,
+  `faixa_salarial` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
--- Copiando dados para a tabela b7web.usuarios: ~2 rows (aproximadamente)
+-- Copiando dados para a tabela b7web.usuarios: ~5 rows (aproximadamente)
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `data_nascimento`) VALUES
-	(7, 'Brendon', 'brendon2@brendon.com', '1234', '1999-12-06'),
-	(8, 'Yago', 'yago@yago.com', '1234', '2019-06-10');
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `data_nascimento`, `contagem`, `faixa_salarial`) VALUES
+	(7, 'Brendon', 'brendon2@brendon.com', '1234', '1999-12-06', 3, 3),
+	(8, 'Yago', 'yago@yago.com', '1234', '2019-06-10', 1, 1),
+	(9, 'Pedro', 'pedro@pedro.com', '1234', '1998-05-25', 2, 2),
+	(10, 'Yasmin', 'yasmin@yasmin.com', '1234', '1994-04-30', 4, 1),
+	(11, 'fantasma', 'fantasma@fantasma.com', '1234', '2000-01-22', 0, 0);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
