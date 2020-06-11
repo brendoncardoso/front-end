a37812ac7c2f02d6c8efc1aed7ad72233f3794c9
Brendon Cardoso <47420059+brendonbr2@users.noreply.github.com>
2020-06-11 13:45:58 -0300

    add projeto loja de allstar em javascript

---
+++ Aulas/b7web/b7web - Javascript 2.0/projeto_allstar/tenis.js
@@ -0,0 +1,9 @@
+let tenisJson = [
+    {id: 1, nome:"Chuck Taylor All Star Hi - Preto", img:"images/1.png", descricao:"Descrição do tenis em mais de uma linha muito legal bem interessante", valor:180.90},
+    {id: 2, nome:"CONVERSE ALL STAR CT AS CORE HI PRETO (CT00040002)", img:"images/2.png", descricao:"Descrição do tenis em mais de uma linha muito legal bem interessante", valor:179.80},
+    {id: 3, nome:"Converse All Star Ilhós Azul-Marinho", img:"images/3.png", descricao:"Descrição do tenis em mais de uma linha muito legal bem interessante", valor:159.60},
+    {id: 4, nome:"Converse All Star CT AS Core OX - Verde", img:"images/4.png", descricao:"Descrição do tenis em mais de uma linha muito legal bem interessante", valor:149.90},
+    {id: 5, nome:"Converse All Star CT AS Core OX (CT0001) - Vermelho", img:"images/5.png", descricao:"Descrição do tenis em mais de uma linha muito legal bem interessante", valor:159.99},
+    {id: 6, nome:"Converse Chuck Taylor All Star Monochrome - Preto", img:"images/6.png", descricao:"Descrição do tenis em mais de uma linha muito legal bem interessante", valor:219.90},
+    {id: 7, nome:"Converse CT AS Core Ox -  Branco", img:"images/7.png", descricao:"Descrição do tenis em mais de uma linha muito legal bem interessante", valor: 159.90},
+];
